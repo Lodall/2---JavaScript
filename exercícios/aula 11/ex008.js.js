16079f8b3d9a726ab77d67f0120088f86b7aacee
Leonardo Lino <leonardo.lino@outlook.com.br>
2022-02-25 20:39:53 -0300

    exercicios

---
+++ exercícios/aula 11/ex008.js.js
@@ -0,0 +1,6 @@
+var vel = 13
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log("Você ultrapassou da velocidade permitida. MULTADO.")
+}
+console.log("Dirija sempre safe.")
