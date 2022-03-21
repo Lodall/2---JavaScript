3d102635da6b498f52598bdc4e7d7216b50b16af
Leonardo Lino <leonardo.lino@outlook.com.br>
2022-03-20 21:30:00 -0300

    update exercicio

--- exercícios/aula 16/ex_analisadorDeNumeros/script.js
+++ exercícios/aula 16/ex_analisadorDeNumeros/script.js
@@ -1,25 +1,41 @@
 function add(){
     let fnumber = document.getElementById('txtn')
     number = Number(fnumber.value)
-    let allNumbers = 0
-    let biggerNumber = ''
-    let smallestNumber = ''
-    let sumOfNumbers = 0
-    let avarage = 0
-    let selec = document.getElementById('selec')
+    allNumbers = 0
+    biggerNumber = 0
+    smallestNumber = ''
+    sumOfNumbers = 0
+    avarage = 0
+    selec = document.getElementById('selec')
+    c = 0
+    res = document.getElementById('res')
 
     if (number > 100 || number <= 0 || number == ''){
         window.alert('Invalid value, please enter a number from 1 to 100.')
     } else{
+        if (number > biggerNumber){
+            biggerNumber = number
+        }
+        if (number == ''){
+            smallestNumber = number
+        } else if(number < smallestNumber){
+            smallestNumber = number
+            
+        }
+        c++
+        sumOfNumbers = number + sumOfNumbers
         let valueOnTheList = document.createElement('option')
         valueOnTheList.text = `Valor ${number} adicionado.`
         selec.appendChild(valueOnTheList)
     
-    }
-
+ 
 }
-
+avarage = sumOfNumbers / c
+}
 function finish(){
-
+    res.innerHTML = `o menor numero é ${smallestNumber} <br> O maior é ${biggerNumber}, a soma deles é ${sumOfNumbers}`
 }
+
+
+
 
